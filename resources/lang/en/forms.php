bed77923edd02a72e38d842a879c0d5446a4d82c
hagerhaf <hafaiedhhager@yahoo.com>
2017-12-21 10:07:51 +0100

    Add files via upload

---
+++ resources/lang/en/forms.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of Hifone.
+ *
+ * (c) Hifone.com <hifone@hifone.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+    'add'      => 'جديد',
+    'save'     => 'حفظ',
+    'publish'  => 'نشر',
+    'update'   => 'تعديل',
+    'create'   => 'جديد',
+    'edit'     => 'تعديل',
+    'delete'   => 'محو',
+    'submit'   => 'ارسال',
+    'cancel'   => 'الغاء',
+    'close'    => 'غلق',
+    'remove'   => 'مسح',
+    'clean'    => 'تنظيف',
+    'preview'  => 'مراجعة',
+    'login'    => 'ولوج',
+    'logout'   => 'خروج',
+    'register' => 'دخول',
+    'enabled'  => 'ممكن',
+    'disabled' => 'غير ممكن',
+];
---
+++ resources/lang/en/hifone.php
@@ -0,0 +1,327 @@
+<?php
+
+/*
+ * This file is part of Hifone.
+ *
+ * (c) Hifone.com <hifone@hifone.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+// Global
+    'home'                => 'الاستقبال',
+    'excellent'           => 'الممتازة',
+    'search'              => 'البحث',
+    'dashboard'           => 'الادمن',
+    'logout'              => 'خروج',
+    'logout_confirm'      => 'هل تود الخروج',
+    'signup'              => 'الدخول',
+    'noitem'              => 'ما من عنصر',
+    'markdown_support'    => '',
+    'at'                  => 'في',
+    'view_count'          => 'الزيارات',
+    'follow'              => 'اتبع',
+    'unfollow'            => 'عدم الاتباع',
+    'favorite'            => 'مفضلة',
+	'Message'            => 'ترك رسالة',
+	'block'            => 'حضر مستعمل',
+	'unblock'            => 'رفع حضر مستعمل',
+    'like'                => 'اعجاب',
+    'unlike'              => 'لا يعجبني',
+    'deleted'             => 'تم المحو',
+    'awesome'             => 'رائع',
+    'whoops'              => 'ايه',
+    'success'             => 'تم',
+    'failure'             => 'لم يتم',
+    'powered_by'          => '',
+    'feed'                => 'جديد',
+    'registered_users'    => 'للمستعملين القدماء',
+    'ranking'             => 'الترتيب',
+    'member'              => 'عضو',
+    'yes'                 => 'نعم',
+    'error_occurred'      => 'حصل خطا',
+    'content_empty'       => 'فارغ',
+    'loading'             => 'تحميل',
+    'uploading_file'      => 'تحميل',
+    'action_title'        => 'التاكيد',
+    'action_text'         => 'متاكد؟',
+// Nodes
+    'nodes'            => [
+        'all'               => 'الافسام',
+        'current'           => 'القسم الحالي',
+        'same_node_threads' => 'الاسئلة',
+    ],
+
+// Threads
+    'threads' => [
+        'threads'        => 'الاسئلة',
+        'title'          => 'عنوان',
+        'body'           => 'محتوى',
+        'add'            => 'سؤال جديد',
+        'list'           => 'الاسئلة',
+        'recent'         => 'حديثة',
+        'excellent'      => 'ممتازة',
+        'like'           => 'محبوبة',
+        'unanswered'     => 'بلا اجوبة',
+        'stick'          => 'Stick',
+        'recommended'    => 'منصوحة بها',
+        'last_reply_by'  => 'من ',
+        'more'           => 'مزيد',
+        'noitem'         => 'ما من اسئلة',
+        'is_excellent'   => 'تم تمييز السؤال',
+        'login_needed'   => '<a class="btn btn-success" href="/auth/login">الولوج</a>',
+        'thread_count'   => '',
+
+        // Share
+        'share2weibo'    => 'Share to weibo',
+        'share2twitter'  => '  Twitter',
+        'share2google'   => ' Google Plus',
+        'share2facebook' => ' Facebook',
+
+        // Create or Edit
+        'pick_node'      => 'Select node',
+        'mark_excellent' => 'Mark excellent',
+        'mark_stick'     => 'Mark stick',
+        'mark_sink'      => 'Mark sink',
+        // Posting Tips
+        'posting_tips'   => [
+            'title'     => '',
+            'pt1_title' => 'العنوان',
+            'pt1_desc'  => 'التدقيق',
+            'pt2_title' => 'القسم',
+            'pt2_desc'  => 'اختر الاقسام المنلسبة',
+            'pt3_title' => 'النص',
+            'pt3_desc'  => '',
+        ],
+        //Community Guidelines
+        'community_guidelines' => [
+            'title'     => '',
+            'cg1_title' => '',
+            'cg1_desc'  => '',
+            'cg2_title' => '',
+            'cg2_desc'  => '',
+        ],
+        'insert_code'  => '',
+        'upload_image' => 'رفع صورة',
+    ],
+
+// Appends
+    'appends' => [
+        'appends' => 'زيادة',
+        'content' => 'محتوى',
+        'notice'  => '',
+    ],
+
+// Favorites
+    'favorites' => [
+        'favorites' => 'مفضلة',
+        'noitem'    => 'ما من مفضلة',
+    ],
+
+// Replies
+    'replies' => [
+        'replies'        => 'اجوبة',
+        'add'            => 'اجابة',
+        'body'           => 'محتوى',
+        'recent'         => 'حديث',
+        'total'          => 'الكلي',
+        'noitem'         => 'ما من اجوبة',
+        'login_required' => 'يجب الولوج',
+    ],
+
+// Photos
+    'photos' => [
+        'drag_drop'     => '',
+        'markdown_desc' => '',
+        'upload_error'  => 'حصل خطا',
+    ],
+
+// Tags
+    'tags' => [
+        'tags'      => 'Tags',
+        'name'      => 'Tag name',
+        'hot'       => 'Hot tags',
+        'tags_help' => 'Comma separated',
+    ],
+
+// Users
+    'users' => [
+        'users'                     => 'المستعملين',
+        'id'                        => 'الرقم',
+        'username'                  => 'اسم المستعمل',
+        'email'                     => 'العنوان الالكتروني',
+        'avatar'                    => 'صورة',
+        'avatar_upload_success'     => 'حملت الصورة',
+        'nickname'                  => 'المستعار',
+        'company'                   => 'الشركة',
+        'score'                     => 'النقاط',
+        'location'                  => 'المدينة',
+        'location_help'             => 'الرجاء الاملاء بالضبط',
+        'blog'                      => 'موقع بلوغ',
+        'block'                     => 'حضر',
+        'unblock'                   => 'عدم حضر',
+        'role'                      => 'دور',
+        'info'                      => 'معلومات',
+        'is_banned'                 => 'مسجون',
+        'total'                     => 'العدد الكلي',
+        'list'                      => 'قائمة',
+        'create'                    => 'جديد',
+        'followers'                 => 'متابعين',
+        'signature'                 => 'امضاء',
+        'bio'                       => 'نبذة',
+        'password'                  => 'كلمة السر',
+        'password_confirmation'     => 'تاكيد كلمة السر',
+        'website'                   => 'موقع',
+        'profile'                   => 'بروفيلي',
+        'favorites'                 => 'مفضلتك',
+        'credits'                   => 'النقاط',
+        'locale'                    => 'اللغة',
+        'edit_profile'              => 'التعديل',
+        'edit_avatar'               => 'تغيير الصورة',
+        'upload_avatar'             => 'صورة جديدة',
+        'upload_avatar_help'        => ' هي انواع الصور المسموحة بها .png & .jpg.الحجم الاقصى هو2 ميقا ',
+        'password_settings'         => 'اعدادات كلمة السر',
+        'password_current'          => 'كلمة السر الحالية',
+        'password_new'              => 'كلمة سر جديد',
+        'password_new_confirmation' => 'تاكيد',
+        'password_update'           => 'تغيير',
+        'select_language'           => 'اللغة',
+        'register_date'             => 'تاريخ الدخول',
+        'add'                       => [
+            'title'   => 'جديد',
+            'success' => 'تمت بنجاح',
+            'failure' => 'حاول مجددا',
+        ],
+        'edit'     => [
+            'title'   => 'اعدادات البروفيل',
+            'success' => 'تم التعديل',
+        ],
+    ],
+
+// Notifications
+    'notifications' => [
+        'my'                          => 'تنبيهاتي',
+        'deleted'                     => 'تم المحو',
+        'noitem'                      => 'ما من تنبيهات',
+        'thread_new_reply'            => 'اجاب على سوال   ',
+        'thread_mention'              => 'تم ذكرك في',
+        'thread_favorite'             => 'تم التفضيل',
+        'thread_follow'               => 'اتبع سوالك',
+        'thread_like'                 => 'اعجب بسوالك',
+        'thread_mark_excellent'       => 'اوصى بسوالك',
+        'thread_move'                 => 'تم تحريك سوالك',
+        'followed_thread_new_reply'   => 'اجاب علي سوالك المفضل',
+        'followed_thread_new_append'  => 'قام باضافات لسوالك',
+        'followed_user_new_thread'    => 'طرح سوالا جديدا',
+        'commented_thread_new_append' => 'قام باضافات جديدة',
+        'user_follow'                 => 'قام باتباعك',
+        'reply_like'                  => 'اعجب بجوابك',
+        'reply_mention'               => 'تم ذكرك في ',
+        'credit_register'             => 'نقاط بالدخول',
+        'credit_login'                => 'ناط بالولوج',
+    ],
+
+// Pms
+    'pms'   => [
+        'pms'             => 'Pm',
+        'create'          => 'Create new Message',
+        'list'            => 'Message List',
+        'home'            => 'Private Messages - Overview',
+        'readed'          => 'Readed',
+        'unreaded'        => 'New Message',
+        'send'            => 'Send Message',
+        'pick_user'       => 'Choose Recipient',
+        'recipient'       => 'Recipient',
+        'recipient_error' => 'Recipient not exists.',
+        'new_pm'          => 'New PM',
+        'view_inbox'      => 'View my Inbox',
+        'nav_create'      => 'Send PM',
+        'nav_inbox'       => 'View Inbox',
+        'nav_outbox'      => 'View Outbox',
+        'same_user_error' => 'Recipient ID and sender ID have the same value.',
+    ],
+
+// Credits
+    'credits' => [
+        'credits'         => 'Credits',
+        'mine'            => 'My credits',
+        'time'            => 'Time',
+        'type'            => 'Type',
+        'reward'          => 'Reward',
+        'balance'         => 'Balance',
+        'balance_current' => 'Current balance',
+    ],
+
+// Tips
+    'tips' => [
+        'tips' => '',
+    ],
+
+// Links
+    'links' => [
+        'links' => 'Useful Links',
+    ],
+
+// Stats
+    'stats' => [
+        'title'   => 'لاحصائيات',
+        'users'   => 'المستعملين',
+        'threads' => 'الاسئلة',
+        'replies' => 'الاجوبة',
+    ],
+
+    'captcha' => [
+        'captcha' => 'CAPTCHA',
+        'refresh' => 'تجديد',
+        'failure' => 'غير صحيح',
+    ],
+
+// Login
+    'login' => [
+        'login'                => 'الدخول',
+        'username'             => 'اسم المستعمل',
+        'login_placeholder'    => 'العتوان الالكتروني',
+        'password'             => 'كلمة السر',
+        'auth_prompt'          => 'الرجاء الولوج',
+        'remember'             => 'تذكرني',
+        'invalid'              => 'معلومات غير صحيحة',
+        'success'              => 'تم الولوج',
+        'success_oauth'        => 'تم الولوج',
+        'tips'                 => '',
+        'account_available'    => '<a href="/auth/login">الولوج</a>.',
+        'login_with_oauth'     => 'الولوج بطرق اخرى',
+        //OAuth Strings
+        'oauth' => [
+            'unbound'         => 'Disconnect from :provider',
+            'unbound_success' => 'تم',
+            'bound'           => 'تم',
+            //OAuth Messages for Login
+            'login' => [
+                'note' => '',
+            ],
+            //OAuth Landing Page
+            'landing' => [
+                'title'    => 'الولوج',
+                'welcome'  => 'الرجاء الاختيار',
+                'choice_1' => ' حساب موجود ',
+                'button_1' => 'ولوج',
+                'choice_2' => 'حساب جديد',
+                'button_2' => 'تسجيل الي',
+                'note'     => '',
+            ],
+            'errors' => [
+                'InvalidState' => 'لم يتم الولوج',
+            ],
+        ],
+    ],
+
+    // Footer
+    'footer' => [
+        'about'   => 'بخصوص',
+        'contact' => 'الاتصال',
+        'faq'     => 'الاسئلة الشائعة',
+    ],
+];
