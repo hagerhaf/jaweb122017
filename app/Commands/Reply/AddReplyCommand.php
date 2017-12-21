d3428ce53987e204b07df9741178f662178707a6
hagerhaf <hafaiedhhager@yahoo.com>
2017-12-21 10:49:44 +0100

    Add files via upload

---
+++ app/Commands/Reply/AddReplyCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of Hifone.
+ *
+ * (c) Hifone.com <hifone@hifone.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Hifone\Commands\Reply;
+
+final class AddReplyCommand
+{
+    public $body;
+	
+	public $credentials;
+
+    public $user_id;
+
+    public $thread_id;
+
+    /**
+     * The validation rules.
+     *
+     * @var string[]
+     */
+    public $rules = [
+        'body'      => 'required|string',
+		'credentials'      => 'required|string',
+        'user_id'   => 'int',
+        'thread_id' => 'int',
+    ];
+
+    /**
+     * Create a new add reply command instance.
+     *
+     * @param string $body
+     */
+    public function __construct($body,$credentials, $user_id, $thread_id)
+    {
+        $this->body = $body;
+        $this->credentials = $credentials;
+        $this->user_id = $user_id;
+        $this->thread_id = $thread_id;
+    }
+}
